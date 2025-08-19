3bfc4fc28c48115b981fce0ad6439a7467d12b49
Alinda Byamukama <alindabyamukama@gmail.com>
2025-08-19 14:03:47 +0300

    add note serializer

--- cases/serializers.py
+++ cases/serializers.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from .model import CaseFile, Note
 
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'case', 'author', 'body', 'created_at']
