dace3585f5225d86fee9b42f7e30f9cade0ea529
Alinda Byamukama <alindabyamukama@gmail.com>
2025-08-19 14:15:25 +0300

    add owner permissions class

--- cases/views.py
+++ cases/views.py
@@ -4,3 +4,7 @@ from rest_framework.exceptions import NotFound
 from django.shortcuts import get_object_or_404
 from .models import CaseFile, Note
 from .serializers import CaseListSerializer, CaseCreateSerializer, NoteSerializer
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return getattr(obj, 'owner_id', None) == request.user.id
