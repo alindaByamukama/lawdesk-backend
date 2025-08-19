b37fe7e083b96cabb00c1736f32206e2e0386d98
Alinda Byamukama <alindabyamukama@gmail.com>
2025-08-19 16:24:29 +0300

    add details and list routes

--- cases/urls.py
+++ cases/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CaseListCreateView, CaseNoteCreateView
+from .views import CaseListCreateView, CaseNoteCreateView, CaseDetailView, CaseNoteListView
 
 urlpatterns = [
     path('cases/', CaseListCreateView.as_view(), name='case-list-create'),
+    path('cases/<int:pk>/', CaseDetailView.as_view(), name='case-detail'),
     path('cases/<int:pk>/notes/', CaseNoteCreateView.as_view(), name='case-note-create'),
+    path('cases/<int:pk>/notes/lists/', CaseNoteListView.as_view(), name='case-note-list'),
 ]
