a41f330f8b71dc3b43058ff07834c09b5baa0fea
Alinda Byamukama <alindabyamukama@gmail.com>
2025-08-19 14:12:08 +0300

    add cases view file

--- cases/views.py
+++ cases/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.shortcuts import get_object_or_404
+from .models import CaseFile, Note
+from .serializers import CaseListSerializer, CaseCreateSerializer, NoteSerializer
